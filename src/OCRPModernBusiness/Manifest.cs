834791980da9b41d49d957e27a3356fd82cfe4a9
psijkof <koffemans@hotmail.com>
2018-09-17 17:08:51 +0100

    added razorpages web as module to main oc cms app

---
+++ src/OCRPModernBusiness/Manifest.cs
@@ -0,0 +1,10 @@
+﻿using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "ModernBusinessRP",
+    Author = "The Orchard Team and friends",
+    Website = "http://orchardproject.net",
+    Version = "0.0.1",
+    Description = "Inspired by Sebastien Ros and Start Bootstrap's Modern Business theme",
+    Category = "Content Management"
+)]
