af21e04b8dd2cb1c086d5c00c0ca224786e88ac6
psijkof <koffemans@hotmail.com>
2018-09-21 15:18:42 +0100

    reboot

--- src/ModernBusinessRP/Manifest.cs
+++
@@ -1,9 +0,0 @@
-using OrchardCore.DisplayManagement.Manifest;
-
-[assembly: Theme(
-    Name = "ModernBusinessRP",
-    Author = "The Orchard Team",
-    Website = "https://orchardproject.net",
-    Version = "0.0.1",
-    Description = "ModernBusinessRP"
-)]
