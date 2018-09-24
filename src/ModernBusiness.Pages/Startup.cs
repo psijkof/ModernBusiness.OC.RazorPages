2c67eedf939e0317f075f6454a568d373e655181
psijkof <koffemans@hotmail.com>
2018-09-24 18:37:30 +0100

    working layout nesting

--- src/ModernBusiness.Pages/Startup.cs
+++ src/ModernBusiness.Pages/Startup.cs
@@ -1,5 +1,7 @@
 using System;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using OrchardCore.Modules;
@@ -10,16 +12,10 @@ namespace ModernBusiness.Pages
     {
         public override void ConfigureServices(IServiceCollection services)
         {
-        }
-
-        public override void Configure(IApplicationBuilder builder, IRouteBuilder routes, IServiceProvider serviceProvider)
-        {
-            routes.MapAreaRoute(
-                name: "Home",
-                areaName: "ModernBusiness.Pages",
-                template: "Home/Index",
-                defaults: new { controller = "Home", action = "Index" }
-            );
+            services.Configure<RazorPagesOptions>(options =>
+            {
+                options.Conventions.AddAreaFolderRoute("ModernBusiness.Pages", "/", "");
+            });
         }
     }
 }
