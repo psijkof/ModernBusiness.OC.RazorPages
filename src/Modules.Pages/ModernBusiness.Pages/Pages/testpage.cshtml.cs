ae7335475b604b2a89f528be2f0219fcd2a21727
psijkof <koffemans@hotmail.com>
2018-09-28 12:20:51 +0100

    index page

---
+++ src/Modules.Pages/ModernBusiness.Pages/Pages/testpage.cshtml.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ModernBusiness.Pages.Pages
+{
+    public class testpageModel : PageModel
+    {
+
+        // public readonly OrchardCore.Flows.ViewModels FlowPartVM;  
+        public void OnGet()
+        {
+
+        }
+    }
+}
