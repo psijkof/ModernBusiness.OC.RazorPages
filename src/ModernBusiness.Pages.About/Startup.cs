c75f8bea6740fc97548f6de32eca73ac9d038f14
Marcos Aidos <Marcos@grupomadigital.onmicrosoft.com>
2018-10-01 14:20:14 +0100

    Added about page

---
+++ src/ModernBusiness.Pages.About/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
+using OrchardCore.Modules;
+
+namespace ModernBusiness.Pages.About
+{
+	public class Startup : StartupBase
+	{
+		public override void ConfigureServices(IServiceCollection services)
+		{
+			services.Configure<RazorPagesOptions>(options =>
+			{
+				options.Conventions.AddAreaFolderRoute("ModernBusiness.Pages.About", "/", "");
+			});
+		}
+	}
+}
