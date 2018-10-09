d6a91f1689b6a70d0aeb22a73882cf59a1fa33b3
psijkof <koffemans@hotmail.com>
2018-10-09 15:22:38 +0100

    language selection basics

---
+++ src/Themes.Pages/ModernBusiness.Theme/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace X4ProKorivarWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpPost]
+        public IActionResult SetLanguage(string culture, string returnUrl)
+        {
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+            );
+            if (string.IsNullOrEmpty(returnUrl)) { returnUrl = "/"; }
+            returnUrl = $"~{returnUrl}";
+            return LocalRedirect(Url.Content(returnUrl));
+        }
+    }
+}
