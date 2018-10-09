9b9c75ea09c9f9a07efd739d5403472fbf99c193
Marcos Aidos <Marcos@grupomadigital.onmicrosoft.com>
2018-10-09 12:13:14 +0100

    still working on tags

--- src/testpart/Assets/tag.js
+++ src/testpart/Assets/tag.js
@@ -1,29 +1,11 @@
-﻿import Multiselect from 'vue-multiselect'
+﻿
 
-export default {
-    components: {
-        Multiselect
-    },
-    data() {
-        return {
-            value: [
-                { name: 'Javascript', code: 'js' }
-            ],
-            options: [
-                { name: 'Vue.js', code: 'vu' },
-                { name: 'Javascript', code: 'js' },
-                { name: 'Open Source', code: 'os' }
-            ]
-        }
-    },
-    methods: {
-        addTag(newTag) {
-            const tag = {
-                name: newTag,
-                code: newTag.substring(0, 2) + Math.floor((Math.random() * 10000000))
-            }
-            this.options.push(tag)
-            this.value.push(tag)
-        }
-    }
-}
+var app = angular.module('plunker', ['ngTagsInput']);
+
+app.controller('MainCtrl', function($scope, $http) {
+  $scope.tags = [
+    { text: 'Tag1' },
+    { text: 'Tag2' },
+    { text: 'Tag3' }
+  ];
+});
