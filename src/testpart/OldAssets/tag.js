8513d1dbc5c00c04ad09b6b6ddbc05b3b4f44c6b
Marcos Aidos <Marcos@grupomadigital.onmicrosoft.com>
2018-10-09 15:36:33 +0100

    working around assets

---
+++ src/testpart/OldAssets/tag.js
@@ -0,0 +1,10 @@
+﻿
+var app = angular.module('plunker', ['ngTagsInput']);
+
+app.controller('MainCtrl', function($scope, $http) {
+  $scope.tags = [
+    { text: 'Tag1' },
+    { text: 'Tag2' },
+    { text: 'Tag3' }
+  ];
+});
