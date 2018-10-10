e7f141198a95a7b8cbf97af11eecbe0eaae7cf3e
Marcos Aidos <Marcos@grupomadigital.onmicrosoft.com>
2018-10-10 12:00:42 +0100

    tag testing, new js file and code

--- src/testpart/wwwroot/Assets/tag.js
+++ src/testpart/wwwroot/Assets/tag.js
@@ -1,11 +1,90 @@
-﻿
+﻿//var app = angular.module('plunker', ['ngTagsInput']);
 
-var app = angular.module('plunker', ['ngTagsInput']);
+//app.controller('MainCtrl', function($scope, $http) {
+//  $scope.tags = [
+//    { text: 'Tag1' },
+//    { text: 'Tag2' },
+//    { text: 'Tag3' }
+//  ];
+//});
+// @ts-check
 
-app.controller('MainCtrl', function($scope, $http) {
-  $scope.tags = [
-    { text: 'Tag1' },
-    { text: 'Tag2' },
-    { text: 'Tag3' }
-  ];
+[].forEach.call(document.getElementsByClassName('tags-input'), function (el) {
+    let hiddenInput = document.createElement('input'),
+        mainInput = document.createElement('input'),
+        tags = [];
+     
+    hiddenInput.setAttribute('type', 'hidden');
+    hiddenInput.setAttribute('name', el.getAttribute('data-name'));
+
+    mainInput.setAttribute('type', 'text');
+    mainInput.setAttribute('asp-for', 'Tags')
+    mainInput.classList.add('main-input');
+    mainInput.addEventListener('input', function () {
+        let enteredTags = mainInput.value.split(',');
+        if (enteredTags.length > 1) {
+            enteredTags.forEach(function (t) {
+                let filteredTag = filterTag(t);
+                if (filteredTag.length > 0) {
+                    addTag(filteredTag);
+                }
+            });
+            mainInput.value = "";
+        }
+    });
+        
+
+    mainInput.addEventListener('keydown', function (e) {
+        let keyCode = e.which || e.keyCode;
+        if (keyCode === 8 && mainInput.value.length === 0 && tags.length > 0) {
+            removeTag(tags.length - 1);
+        }
+    });
+
+    el.appendChild(mainInput);
+    el.appendChild(hiddenInput);
+
+
+    function addTag(text) {
+        let tag = {
+            text: text,
+            element: document.createElement('span')
+        };
+
+        tag.element.classList.add('tag');
+        tag.element.textContent = tag.text;
+
+        let closeBtn = document.createElement('span');
+        closeBtn.classList.add('close');
+        closeBtn.addEventListener('click', function () {
+            removeTag(tags.indexOf(tag));
+        });
+        tag.element.appendChild(closeBtn);
+
+        tags.push(tag);
+
+        el.insertBefore(tag.element, mainInput);
+
+        refreshTags();
+    }
+
+    function removeTag(index) {
+        let tag = tags[index];
+        tags.splice(index, 1)
+        el.removeChild(tag.element);
+        refreshTags();
+
+    }
+
+    function refreshTags() {
+        let tagsList = []
+        tags.forEach(function (t) {
+            tagsList.push(t.text);
+        });
+        hiddenInput.value = tagsList.join(',');
+    }
+
+    function filterTag(tag) {
+        return tag.replace(/[^\w -]/g, '').trim().replace(/\W+/g, '-');
+    }
 });
