6cccf432acdd9a595cc709959e9fa3d0d924cc22
psijkof <koffemans@hotmail.com>
2018-10-15 16:14:58 +0100

    priceplan localization complete

--- src/Modules.Pages/ModernBusiness.Pages.PricePlan/Pages/Price-Plan.cshtml.cs
+++ src/Modules.Pages/ModernBusiness.Pages.PricePlan/Pages/Price-Plan.cshtml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Localization;
+using ModernBusiness.Pages.Shared.ViewModels;
 using OrchardCore;
 using OrchardCore.ContentManagement;
 
@@ -12,18 +14,26 @@ namespace ModernBusiness.Pages.PricePlan
     public class Price_PlanModel : PageModel
     {
         private readonly IOrchardHelper _orchard;
+        private readonly IStringLocalizer T;
 
-        public Price_PlanModel(IOrchardHelper orchard)
+        public Price_PlanModel(IOrchardHelper orchard, IStringLocalizer<Price_PlanModel> t)
         {
             _orchard = orchard;
+            T = t;
         }
 
         public ContentItem Pricing { get; private set; }
+        public BreadCrumbViewModel BreadCrumbVM { get; private set; }
         public IEnumerable<ContentItem> PriceList { get; private set; }
 
         public async void OnGet()
         {
             Pricing = (await _orchard.GetRecentContentItemsByContentTypeAsync("Pricing")).SingleOrDefault();
+            BreadCrumbVM = new BreadCrumbViewModel()
+            {
+                DisplayText = T[Pricing.DisplayText],
+                SubTitle = T[(string)Pricing.Content.SubtitlePart?.Subtitle.Text ?? ""]
+            };
             PriceList = (await _orchard.GetRecentContentItemsByContentTypeAsync("PricePlan")).OrderBy(p => p.Content.PricePlan.Price.Value);
         }
     }
---
+++ src/Modules.Pages/ModernBusiness.Pages.Shared/ViewModels/BreadCrumbViewModel.cs
@@ -0,0 +1,14 @@
+﻿using OrchardCore.ContentManagement;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModernBusiness.Pages.Shared.ViewModels
+{
+    public class BreadCrumbViewModel
+    {
+        public string DisplayText { get; set; }
+        public string SubTitle { get; set; }
+        public ContentItem Content { get; set; } = null;
+    }
+}
