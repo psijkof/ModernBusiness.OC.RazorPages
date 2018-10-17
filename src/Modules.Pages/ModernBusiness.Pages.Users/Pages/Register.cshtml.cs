383336d3d4c2336ff97357a546bdcd4eadfa6cc0
Marcos Aidos <Marcos@grupomadigital.onmicrosoft.com>
2018-10-17 16:49:02 +0100

    Added register user page, working with simples example

--- src/Modules.Pages/ModernBusiness.Pages.Users/Pages/Register.cshtml.cs
+++ src/Modules.Pages/ModernBusiness.Pages.Users/Pages/Register.cshtml.cs
@@ -4,7 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using OrchardCore.Users;
 using ModernBusiness.Pages.Users.ViewModels;
+using OrchardCore.Users.Services;
+using OrchardCore.Users.Models;
+using Microsoft.Extensions.Logging;
 
 namespace ModernBusiness.Pages.Users.Pages
 {
@@ -13,9 +17,14 @@ namespace ModernBusiness.Pages.Users.Pages
 		[BindProperty]
 		public RegisterViewModel RegisterVM { get; set; }
 
-		public RegisterModel()
+		ILogger _logger;
+
+		private readonly IUserService _userService;
+
+		public RegisterModel(IUserService userService, ILogger<RegisterModel> logger)
 		{
-			// 
+			_userService = userService;
+			_logger = logger;
 		}
 
         public async Task<IActionResult> OnGetAsync(string returnUrl)
@@ -25,10 +34,30 @@ namespace ModernBusiness.Pages.Users.Pages
 			return Page();
         }
 
-		//public async Taks<IActionResult> OnPostAsync()
-		//{
+		public async Task<IActionResult> OnPostAsync(string returnUrl)
+		{
+			ViewData["ReturnUrl"] = returnUrl;
 
-		//}
-		
-    }
+			if (ModelState.IsValid)
+			{
+				returnUrl = returnUrl ?? Url.Content("~/");
+
+				var user = await _userService.CreateUserAsync(new User
+				{
+					UserName = RegisterVM.Name,
+					Email = RegisterVM.Email,
+					RoleNames = new string[0]
+				}, RegisterVM.Password, (key, message) => ModelState.AddModelError(key, message)) as User;
+
+				if (user != null)
+				{
+					_logger.LogDebug("Hello{0}!!!", user.UserName);
+					_logger.LogInformation(3, "User created!");
+					return LocalRedirect(returnUrl);
+				}
+			}
+
+			return Page();
+		}
+	}
 }
