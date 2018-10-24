566645a1bd232ff88092586a88d4b2a0630ee906
Marcos Aidos <Marcos@grupomadigital.onmicrosoft.com>
2018-10-24 18:19:00 +0100

    new theme module

---
+++ src/Themes.Pages/ModernBusiness.Theme2/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "ModernBusiness.Theme2",
+    Author = "The Orchard Team",
+    Website = "https://orchardproject.net",
+    Version = "0.0.1",
+    Description = "ModernBusiness.Theme2"
+)]
