278547b3a52e96ad00a38d2831e9e5e992afbfd2
Sijmen Koffeman <Sijmen.Koffeman@hitachiconsulting.com>
2019-04-26 15:43:28 +0100

    updated to latest version of dev branch, added markdowntohtml helper in blogpost.cshtml

--- src/Themes.Pages/ModernBusiness.Theme/Manifest.cs
+++ src/Themes.Pages/ModernBusiness.Theme/Manifest.cs
@@ -1,9 +1,9 @@
 using OrchardCore.DisplayManagement.Manifest;
 
 [assembly: Theme(
-    Name = "ModernBusiness.Theme",
+    Name = "The default ModernBusiness theme",
     Author = "The Orchard Team",
     Website = "https://orchardproject.net",
     Version = "0.0.1",
-    Description = "ModernBusiness.Theme"
+    Description = "Theme is created by Start Bootstrap"
 )]
